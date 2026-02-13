f8961477cc8ac0306991badef7ca7bb7d6e7cbc3
yuuuuujing <149015966+yuuuuujing@users.noreply.github.com>
2026-02-13 13:36:40 +0000

    added more menu items

--- src/main/java/com/foodtruck/Menu.java
+++ src/main/java/com/foodtruck/Menu.java
@@ -11,8 +11,10 @@ public class Menu {
         items = new HashMap<>();
 
         items.put("salad_greek", new Salad("Greek Salad", 7.50));
+        items.put("salad_caesar", new Salad("Caesar Salad", 12.50));
         items.put("sandwich_turkey", new Sandwich("Turkey Sandwich", 9.50));
-        items.put("drink_water", new Drink("Water", 1.50));
+        items.put("sandwich_chicken", new Sandwich("Chicken Sandwich", 5.60));
+        items.put("drink_soda", new Drink("Soda", 1.50));
     }
 
     public HashMap<String, MenuItem> getAllItems() {
